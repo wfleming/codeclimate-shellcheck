b6c2a95d219ad8c690448aaba427956e3dbece40
Philip Cunningham <philip@irisconnect.co.uk>
2015-09-11 13:41:05 +0100

    begin fleshing out issue type.
    An `issue` represents a single instance of a real or potential code problem, detected by a static analysis Engine.
    
    ```json
    {
      "type": "issue",
      "check_name": "Bug Risk/Unused Variable",
      "description": "Unused local variable `foo`",
      "categories": ["Complexity"],
      "location": Location,
      "remediation_points": 500,
      "content": Content,
      "other_locations": [Location]
    }
    ```
    
    * `type` -- **Required**. Must always be "issue".
    * `check_name` -- **Required**. A unique name representing the static analysis check that emitted this issue.
    * `description` -- **Required**. A string explaining the issue that was detected.
    * `categories` -- **Required**. At least one category indicating the nature of the issue being reported.
    * `location` -- **Required**. A `Location` object representing the place in the source code where the issue was discovered.
    * `remediation_points` -- **Optional**. An abstract, relative integer indicating a rough estimate of how long it would take to resolve the reported issue.
    * `content` -- **Optional**. A markdown snippet describing the issue, including deeper explanations and links to other resources.
    * `other_locations` -- **Optional.** An array of `Location` objects useful for engines which highlight more than one source location in an issue.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+-- Types.
+
+data Location
+data Content
+
+data Issue = Issue {
+    _type               :: String
+  , _check_name         :: String
+  , _description        :: String
+  , _categories         :: [String]
+  , _location           :: Location
+  , _remediation_points :: Int
+  , _content            :: Content
+  , _other_locations    :: [Location]
+}
+
 main :: IO ()
 main = putStrLn "hey"

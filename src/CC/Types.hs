7b1bb1242ea1aec00d3eb4e6ba8325beb0a10124
Philip Cunningham <philip@irisconnect.co.uk>
2015-09-12 15:05:44 +0100

    add some comments from spec doc.

--- src/CC/Types.hs
+++ src/CC/Types.hs
@@ -8,18 +8,7 @@ import Data.Aeson.Types (Pair, Value(Null), defaultOptions, fieldLabelModifier)
 import Data.Char        (isUpper, toLower)
 import GHC.Generics     (Generic)
 
-data BeginEnd = Positions Position Position
-              | Lines Int Int
-              deriving Show
-
-instance ToJSON BeginEnd where
-  toJSON z = object $ case z of
-    Positions x y -> f x y
-    Lines     x y -> f x y
-    where
-      f :: (ToJSON a) => a -> a -> [Pair]
-      f x y = [ "begin" .= x, "end" .= y ]
-
+-- | Issues must be associated with one or more categories.
 data Category = BugRisk
               | Clarity
               | Compatibility
@@ -33,6 +22,54 @@ instance ToJSON Category where
   toJSON BugRisk = "Bug Risk"
   toJSON x       = toJSON $ show x
 
+-- | Line and column numbers are 1-based.
+data LineColumn = LineColumn {
+    _line   :: Int
+  , _column :: Int
+} deriving (Generic, Show)
+
+instance ToJSON LineColumn where
+  toJSON = genericToJSON defaultOptions { fieldLabelModifier = drop 1 }
+
+-- | PositionBased refer to specific characters within a source file, and can be
+-- expressed in two ways.
+data Position = Coords LineColumn
+                -- ^ Line and column coordinates.
+              | Offset Int
+                -- ^ Absolute character offsets, for the entire source buffer.
+              deriving Show
+
+instance ToJSON Position where
+  toJSON (Coords x) = toJSON x
+  toJSON (Offset x) = object [ "offset" .= x ]
+
+-- | Line-based locations emit a beginning and end line number for the issue,
+-- whereas position-based locations allow more precision.
+data BeginEnd = PositionBased Position Position
+              | LineBased Int Int
+              deriving Show
+
+instance ToJSON BeginEnd where
+  toJSON z = object $ case z of
+    PositionBased x y -> f x y
+    LineBased     x y -> f x y
+    where
+      f :: (ToJSON a) => a -> a -> [Pair]
+      f x y = [ "begin" .= x, "end" .= y ]
+
+-- | Locations refer to ranges of a source code file.
+data Location = Location FilePath BeginEnd deriving Show
+
+instance ToJSON Location where
+  toJSON (Location x y) = object $ case y of
+    PositionBased _ _ -> [ f x, "positions" .= y ]
+    LineBased _ _     -> [ f x, "lines" .= y ]
+    where
+      f :: FilePath -> Pair
+      f = (.=) "path"
+
+-- | An issue represents a single instance of a real or potential code problem,
+-- detected by a static analysis Engine.
 data Issue = Issue {
     _check_name         :: String
   , _description        :: String
@@ -57,29 +94,3 @@ instance ToJSON Issue where
     where
       withoutNulls :: [(a, Value)] -> [(a, Value)]
       withoutNulls = filter (\(_, v) -> v /= Null)
-
-data LineColumn = LineColumn {
-    _line   :: Int
-  , _column :: Int
-} deriving (Generic, Show)
-
-instance ToJSON LineColumn where
-  toJSON = genericToJSON defaultOptions { fieldLabelModifier = drop 1 }
-
-data Location = Location FilePath BeginEnd deriving Show
-
-instance ToJSON Location where
-  toJSON (Location x y) = object $ case y of
-    Positions _ _ -> [ f x, "positions" .= y ]
-    Lines _ _     -> [ f x, "lines" .= y ]
-    where
-      f :: FilePath -> Pair
-      f = (.=) "path"
-
-data Position = Coords LineColumn
-              | Offset Int
-              deriving Show
-
-instance ToJSON Position where
-  toJSON (Coords x) = toJSON x
-  toJSON (Offset x) = object [ "offset" .= x ]

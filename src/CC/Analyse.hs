7c0762d0dadc8d26fb8035ae0e991395a03398bb
Philip Cunningham <philip@irisconnect.co.uk>
2015-09-16 17:20:23 +0100

    minor stylistic changes

--- src/CC/Analyse.hs
+++ src/CC/Analyse.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module CC.Analyse (
-  analyse
-) where
+module CC.Analyse where
 
 import Control.Applicative
 
@@ -11,39 +9,24 @@ import CC.Types as CC
 import Control.Exception.Base (IOException, catch)
 import Data.Monoid            ((<>))
 import ShellCheck.Checker     (checkScript)
-import ShellCheck.Interface   (CheckResult(..),
-                               CheckSpec(..),
-                               Comment(..),
-                               ErrorMessage,
-                               Position(..),
-                               PositionedComment(..),
-                               Severity(..),
-                               SystemInterface(..))
+import ShellCheck.Interface   (CheckResult(..)
+                             , CheckSpec(..)
+                             , Comment(..)
+                             , ErrorMessage
+                             , Position(..)
+                             , PositionedComment(..)
+                             , Severity(..)
+                             , SystemInterface(..))
 
 -- | Main function that analyses shell files.
 analyse :: FilePath -> IO [Issue]
-analyse path = do
-  contents <- readFile path
-  result <- checkScript ioInterface (toCheckSpec path contents)
-  return $ toIssues result
+analyse x = do
+  y <- readFile x
+  z <- checkScript interface (defaultCheckSpec { csFilename = x, csScript = y })
+  return $ transform z
   where
-    ioInterface = SystemInterface { siReadFile = defaultInterface }
-
--- | Lifts path and body into CheckSpec.
-toCheckSpec :: FilePath -> String -> CheckSpec
-toCheckSpec path contents = CheckSpec {
-      csFilename = path
-    , csScript = contents
-    , csExcludedWarnings = []
-    , csShellTypeOverride = Nothing
-}
-
--- | Builds default IO interface with error handling.
-defaultInterface :: FilePath -> IO (Either ErrorMessage String)
-defaultInterface x = (Right <$> readFile x) `catch` handler
-  where
-    handler :: IOException -> IO (Either ErrorMessage String)
-    handler ex = return . Left $ show ex
+    interface :: SystemInterface IO
+    interface = SystemInterface { siReadFile = defaultInterface }
 
 -- | Maps Severity to Category.
 categorise :: Severity -> Category
@@ -52,12 +35,28 @@ categorise InfoC    = BugRisk
 categorise StyleC   = Style
 categorise WarningC = BugRisk
 
--- | Maps CheckResult into issues.
-toIssues :: CheckResult -> [Issue]
-toIssues CheckResult{..} = fmap toIssue crComments
+-- | Builds default, empty, CheckSpec.
+defaultCheckSpec :: CheckSpec
+defaultCheckSpec = CheckSpec {
+      csFilename = ""
+    , csScript = ""
+    , csExcludedWarnings = []
+    , csShellTypeOverride = Nothing
+}
+
+-- | Builds default IO interface with error handling.
+defaultInterface :: FilePath -> IO (Either ErrorMessage String)
+defaultInterface x = catch (Right <$> readFile x) handler
   where
-    toIssue :: PositionedComment -> Issue
-    toIssue (PositionedComment Position{..} (Comment severity code description)) =
+    handler :: IOException -> IO (Either ErrorMessage String)
+    handler ex = return . Left $ show ex
+
+-- | Maps CheckResult into issues.
+transform :: CheckResult -> [Issue]
+transform CheckResult{..} = fmap f crComments
+  where
+    f :: PositionedComment -> Issue
+    f (PositionedComment Position{..} (Comment severity code description)) =
       Issue {
           _check_name         = "ShellCheck/" <> show code
         , _description        = description

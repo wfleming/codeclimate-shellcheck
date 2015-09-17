83d9e55db1a4537c30d87ac82c301bd4e74d2b8d
Philip Cunningham <philip@irisconnect.co.uk>
2015-09-17 17:46:38 +0100

    begin adding config.json functionality.

--- src/Main.hs
+++ src/Main.hs
@@ -6,21 +6,32 @@ module Main where
 import CC.Analyse
 import CC.Types
 import Control.Applicative
-import Data.Aeson           (encode)
+import Data.Aeson           (FromJSON, decode, encode)
+import Data.Maybe           (fromMaybe)
 import Data.Monoid          ((<>))
-import System.Directory     (getCurrentDirectory)
+import System.Directory     (doesFileExist, getCurrentDirectory)
 import System.FilePath.Glob (compile, globDir)
 
 import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
-main = shFiles >>= analyseFiles >>= mapM_ printIssue
+main = do
+  x <- loadConfig "/config.json"
+  y <- shFiles
+  z <- analyseFiles ((_include_paths x) ++ y)
+  mapM_ printIssue z
+
+loadConfig :: FilePath -> IO Config
+loadConfig x = do
+    y <- doesFileExist x
+    z <- if y then decode <$> BL.readFile x else return Nothing
+    return $ fromMaybe (Config []) z
 
 printIssue :: Issue -> IO ()
 printIssue = BL.putStr . (<> "\0") . encode
 
 shFiles :: IO [FilePath]
-shFiles = map clean . concat . fst <$> globDir [compile "**/*.sh"] "."
+shFiles = fmap clean . concat . fst <$> globDir [compile "**/*.sh"] "."
   where
     clean :: [Char] -> [Char]
     clean ('.' : '/' : x) = x

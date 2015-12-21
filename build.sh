b427e639553be3e1ddb80a19d5c8bd06187cc360
Philip Cunningham <hello@filib.io>
2015-12-21 12:21:48 +0000

    use libgmp compiled with glibc instead of musl to prevent issues further down the line.

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 docker build -t codeclimate-shellcheck-build:latest -f Build.plan .
 
 # Kick off the build process.
-docker run --rm -v $PWD/.local:/root/.local               \
-                -v $PWD/.stack-work:/home/app/.stack-work \
-                -v $HOME/.stack:/root/.stack              \
-                codeclimate-shellcheck-build:latest stack install
+docker run --rm -v $PWD/.local:/root/.local                               \
+                -v $PWD/.stack-work:/home/app/.stack-work                 \
+                -v $HOME/.stack:/root/.stack                              \
+                codeclimate-shellcheck-build:latest                       \
+                cp /usr/lib/x86_64-linux-gnu/libgmp.so.10 /root/.local && \
+                stack install --force-dirty

6733f346e10ebb281929281d332aefc8ae0fb685
Philip Cunningham <hello@filib.io>
2015-12-22 19:08:54 +0000

    update data on each build.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Prepare the data mappings.
+git submodule init
+git submodule update
+./data/prepare.rb
+
 # Build the build container.
 docker build -t codeclimate-shellcheck-build:latest -f "$PWD/docker/Build.plan" .
 

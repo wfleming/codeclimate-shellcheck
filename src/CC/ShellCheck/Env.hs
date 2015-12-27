15fd63515376ebe108c97409324ba42c312d916e
Philip Cunningham <hello@filib.io>
2015-12-27 13:28:28 +0000

    restructure computation to decouple reporting and analysing.
    in brief:
    
    a channel parameterised by the analysis type, can be in three states. a
    result, complete or reported.
    
    * result
    an issue that came back from shellcheck.
    
    * complete
    all scripts have been analysed but not necessarily reported.
    
    * reported
    all scripts have been analysed and reported.
    
    previously we were accumulating all of the results before iterating and
    printing them. this could come become very unsatisfactory when we have
    large repos with many hundreds of scripts for analysis.

--- src/CC/ShellCheck/Env.hs
+++ src/CC/ShellCheck/Env.hs
@@ -6,9 +6,12 @@ import           CC.ShellCheck.Types
 import qualified Data.Map.Strict as DM
 import           Data.Maybe
 import qualified Data.Yaml as YML
+import           System.Directory
 
 --------------------------------------------------------------------------------
 
 -- | Load environment that maps remediation points to textual content.
 loadEnv :: IO Env
-loadEnv = fromMaybe DM.empty <$> YML.decodeFile "./data/env.yml"
+loadEnv = do
+  path <- getCurrentDirectory >>= \cwd -> return (cwd ++ "/data/env.yml")
+  fromMaybe DM.empty <$> YML.decodeFile path

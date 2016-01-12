ef32babac6fcfa40cf47e8c9b3ee888ec2b09a8d
mrb <michaelrbernstein@gmail.com>
2016-01-12 15:49:13 -0500

    Add b

--- docker/release.sh
+++ docker/release.sh
@@ -4,4 +4,4 @@
 docker run -v "$PWD/.local/bin:/data" lalyos/upx codeclimate-shellcheck
 
 # Create the engine container.
-docker build -t "$CODECLIMATE_ENGINE_REGISTRY/$CODECLIMATE_ENGINE_NAME:$CODECLIMATE_ENGINE_BUILD_NUM" -f "$PWD/docker/Release.plan" .
+docker build -t "$CODECLIMATE_ENGINE_REGISTRY/$CODECLIMATE_ENGINE_NAME:b$CODECLIMATE_ENGINE_BUILD_NUM" -f "$PWD/docker/Release.plan" .
